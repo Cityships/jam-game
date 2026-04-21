476c8273211f2f134b8a8cd75612d543a0eb9c7b
Alex <vysllian@gmail.com>
2026-04-21 15:02:26 -0500

    adding a 3rd person camera controller

---
+++ main_ui.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+@export var pause_display : Control
+
+var toggle_mouse_capture : bool:
+    set(value):
+        toggle_mouse_capture = value
+        if toggle_mouse_capture:
+            Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+        else:
+            Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _input(_event: InputEvent) -> void:
+    if Input.is_action_just_pressed("pause_game"):
+        get_tree().paused = !get_tree().paused
+        pause_display.visible = !pause_display.visible
+    if Input.is_action_just_pressed("toggle_mouse_capture"):
+        toggle_mouse_capture = !toggle_mouse_capture
